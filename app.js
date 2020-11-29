eecd9b510b4f72aa940469eab01f64e6a7e32275
Bruno Kristins <brunokristins@gmail.com>
2020-11-29 09:37:55 +0000

    final version

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const port = 3000;
 const authRouter = require('./routes/admin/auth');
-const productsRouter = require('./routes/admin/products');
+const adminProductsRouter = require('./routes/admin/products');
+const productsRouter = require('./routes/products');
+const cartsRouter = require('./routes/carts');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -12,7 +14,9 @@ app.use(cookieSession({
     keys: ['lkasdasdadasdas65dd67']
 }));
 app.use(authRouter);
+app.use(adminProductsRouter);
 app.use(productsRouter);
+app.use(cartsRouter);
 
 
 app.listen(port, () => {
